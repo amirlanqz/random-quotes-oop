09ab3c1c1703fa13987e4d97690bc8fbd5430fd0
amirlanqz <amirlanqz@gmail.com>
2025-03-04 11:55:09 +0500

    Initial version of the quotes app using OOP and classes - chosing random quote from the array of quotes

---
+++ src/data/quotes.js
@@ -0,0 +1,64 @@
+const quotes = [
+  {
+    id: 'a985aaf5',
+    text: 'Do what you can, with what you have, where you are.',
+    author: 'Theodore Roosevelt',
+    isFavorite: false,
+  },
+  {
+    id: 'cf3c06b1',
+    text: 'Success is not final, failure is not fatal: it is the courage to continue that counts.',
+    author: 'Winston Churchill',
+    isFavorite: false,
+  },
+  {
+    id: '72ceea28',
+    text: 'In the middle of every difficulty lies opportunity.',
+    author: 'Albert Einstein',
+    isFavorite: false,
+  },
+  {
+    id: '80d1ca79',
+    text: 'It does not matter how slowly you go as long as you do not stop.',
+    author: 'Confucius',
+    isFavorite: false,
+  },
+  {
+    id: 'ce637278',
+    text: "Don't watch the clock; do what it does. Keep going.",
+    author: 'Sam Levenson',
+    isFavorite: false,
+  },
+  {
+    id: '5036783c',
+    text: 'Strive not to be a success, but rather to be of value.',
+    author: 'Albert Einstein',
+    isFavorite: false,
+  },
+  {
+    id: 'e3a998fb',
+    text: "Believe you can and you're halfway there.",
+    author: 'Theodore Roosevelt',
+    isFavorite: false,
+  },
+  {
+    id: 'f6f1bc89',
+    text: 'The best way to predict the future is to create it.',
+    author: 'Peter Drucker',
+    isFavorite: false,
+  },
+  {
+    id: 'a1b9b969',
+    text: 'It always seems impossible until it’s done.',
+    author: 'Nelson Mandela',
+    isFavorite: false,
+  },
+  {
+    id: '038310a9',
+    text: 'Act as if what you do makes a difference. It does.',
+    author: 'William James',
+    isFavorite: false,
+  },
+];
+
+export default quotes;
